e88fae74457ebd2c49f4852cc125e10aab84dd59
irmak23 <irmakkose@posta.mu.edu.tr>
2025-04-24 13:17:47 +0300

    Add files via upload

--- Abstract/src/Rectangle.java
+++ Abstract/src/Rectangle.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+public class Rectangle extends Shape{
+    private int sideA = 0 ,sideB = 0;
 
-public class Rectangle {
+    public Rectangle(){
+
+    }
+
+
+    public Rectangle(int sideA, int sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    public Rectangle(String color, int sideA, int sideB) {
+        super(color);
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2*(sideA+sideB);
+    }
+
+
+    @Override
+    public double area() {
+        return sideA*sideB;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +"Rectangle{" +
+                "SideA=" + sideA +
+                ", SideB=" + sideB +
+                '}';
+    }
 }
--- Abstract/src/TestAbstractShapes.java
+++ Abstract/src/TestAbstractShapes.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
+import java.util.ArrayList;
 public class TestAbstractShapes {
+    public static void main(String[] args) {
+        Shape rectangle = new Rectangle(3, 4);
+        System.out.println(rectangle.area());
+        System.out.println(rectangle.perimeter());
+        System.out.println(rectangle);
+
+        Rectangle rectangle1 = new Rectangle(3,4);
+        System.out.println(rectangle1.area());
+        System.out.println(rectangle1.perimeter());
+        System.out.println(rectangle1);
+
+        Shape square = new Rectangle(3,3);
+        System.out.println(square.area());
+        System.out.println(square.perimeter());
+        System.out.println(square);
+
+        ArrayList<Shape> shapes = new ArrayList<Shape>();
+        shapes.add(rectangle);
+        shapes.add(rectangle1);
+        shapes.add(square);
+
+        for(int i = 0; i < 3; i++){
+            System.out.println(shapes.get(i));
+        }
+
+
+
+
+
+
+
+    }
 }
